2ca022463045e74c51577253147b029d4404cf4b
Nauel Gómez <ngomez@codiara.com>
2026-05-11 20:29:16 +0200

    Initial setup TS con app

---
+++ src/main.ts
@@ -0,0 +1,5 @@
+
+const app = document.querySelector<HTMLHeadingElement>('#app');
+// Null-safe control
+if (!app) throw new Error('No se encontró #app');
+app.textContent = 'Bienvenidos a Quidditch Champions XX';
