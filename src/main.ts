ec7ecf309149e6e7485a5f15bffb43424b3e959c
Nauel Gómez <ngomez@codiara.com>
2026-05-11 20:42:25 +0200

    Add utils.ts y clean de dist y cache

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,8 @@
+import { greet } from './utils';
+
 
 const app = document.querySelector<HTMLHeadingElement>('#app');
 // Null-safe control
 if (!app) throw new Error('No se encontró #app');
-app.textContent = 'Bienvenidos a Quidditch Champions XX';
+// app.textContent = 'Bienvenidos a Quidditch Champions XX';
+app.textContent = greet('Hermione');
