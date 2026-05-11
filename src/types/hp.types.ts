7c895fec5289c91ebf2355bd84cc1449a2c6fca2
Nauel Gómez <ngomez@codiara.com>
2026-05-11 21:43:49 +0200

    HP API types

---
+++ src/types/hp.types.ts
@@ -0,0 +1,10 @@
+// Contrato de la api de Harry Potter
+// URL: https://hp-api.onrender.com/
+
+export interface HPCharacter {
+    id: string;
+    name: string;
+    house: string;
+    alive: boolean;
+    image: string;
+}
