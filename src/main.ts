de391e47972ec8b9709c9fc4b42e5953d5fc95a1
Nauel Gómez <ngomez@codiara.com>
2026-05-19 21:04:52 +0200

    Page class 1

--- src/main.ts
+++ src/main.ts
@@ -1,93 +1,11 @@
-import { greet } from './utils';
-import { pickRandomHouse } from './random';
-import { getCharacters } from './services/hp-api';
-import type { HPCharacter } from './types/hp.types';
 import { Page } from './pages/Page';
 
-type RenderOptions = {
-    house?: string;
-    limit?: number;
-}
 
-type CharacterState = 
-    | { status: 'idle' }
-    | { status: 'loading' }
-    | { status: 'success', data: HPCharacter[] }
-    | { status: 'error', message: string };
-
-let state: CharacterState = { status: 'idle' };
-
-function render(): void {
-    const app = document.querySelector<HTMLHeadingElement>('#app');
-    if (!app) return;
-
-    switch (state.status) {
-        case 'idle':    app.textContent = 'Pendiente invocar API HP'; break;
-        case 'loading': app.textContent = 'Cargando personajes...'; break;
-        case 'error':   app.textContent = `ERROR: ${state.message}`; break;
-        case 'success':
-            app.textContent = `${state.data.length} personajes cargados`;
-            renderCharacters(state.data);
-            break;
-    }
-};
-
-function renderCharacters(characters: HPCharacter[], options: RenderOptions = {}): void {
-    const list = document.querySelector<HTMLUListElement>('#characters');
-    if (!list) throw new Error('No se encontró #characters');
-
-    // TODO: Amplia esta función para pintar la imagen ANTES de cada elemento LI
-
-    list.replaceChildren();
-
-    // Filters
-    let filtered = characters;
-    filtered = filtered.filter(i => i.image);
-
-    if (options.house) {
-        filtered = filtered.filter(i => i.house === options.house);
-    }
-
-    if (options.limit) {
-        filtered = filtered.slice(0, options.limit);
-    }
-
-    // End Filters
-
-    for (const character of filtered) {
-        const li = document.createElement('li');
-        li.textContent = `${character.name} - ${character.house || 'Sin Casa' }`;
-
-        // Foto:
-        const img = document.createElement('img');
-        img.src = character.image;
-        img.alt = character.name;
-        img.width = 50;
-        li.prepend(img);
-
-        list.appendChild(li);
-    }
-
-}
-
-async function load(): Promise<void> {
-    state = { status: 'loading' };
-    render();
-    try {
-        const data = await getCharacters();
-        state = { status: 'success', data: data };
-    } catch (e: unknown) {
-        state = { status: 'error', message: e instanceof Error ? e.message : 'Unknown error' }
-    }
-    render();
-}
-
-load();
 
 // Identificar cual es el selector principal de HTML
 const app = document.querySelector<HTMLHeadingElement>('#app');
 if (!app) throw new Error('No se encontró #app');
 // Instanciar la clase correspondiente a la pagina
-const home = new Page(); // HomePage();
+const home = new Page(app); // HomePage();
 // Llamar al método principal de esa pagina
 home.mount();
--- src/pages/Page.ts
+++ src/pages/Page.ts
@@ -1,37 +1,89 @@
+import { getCharacters } from '../services/hp-api';
 import type { HPCharacter, CharacterState } from '../types/hp.types';
+
+type RenderOptions = {
+    house?: string;
+    limit?: number;
+}
 
 export class Page {
 
     private slug: string = '/';
     private title: string = 'Quidditch Champions - Home';
+    private readonly root: HTMLElement;
     private state: CharacterState;
 
-    constructor() {
+    constructor(
+        root: HTMLElement
+    ) {
+        this.root = root;
         this.state = { status: 'idle' };
     }
 
     private render(): Promise<void> | void {
-        const app = document.querySelector<HTMLHeadingElement>('#app');
-
-        if (!app) return;
 
         switch (this.state.status) {
-            case 'idle':    app.textContent = 'Pendiente invocar API HP'; break;
-            case 'loading': app.textContent = 'Cargando personajes...'; break;
-            case 'error':   app.textContent = `ERROR: ${this.state.message}`; break;
+            case 'idle':    this.root.textContent = 'Pendiente invocar API HP'; break;
+            case 'loading': this.root.textContent = 'Cargando personajes...'; break;
+            case 'error':   this.root.textContent = `ERROR: ${this.state.message}`; break;
             case 'success':
-                app.textContent = `${this.state.data.length} personajes cargados`;
-                // renderCharacters(state.data);
+                this.root.textContent = `${this.state.data.length} personajes cargados`;
+                this.renderCharacters(this.state.data);
                 break;
         }
     };
 
+    private renderCharacters(characters: HPCharacter[], options: RenderOptions = {}): void {
+    const list = document.querySelector<HTMLUListElement>('#characters');
+    if (!list) throw new Error('No se encontró #characters');
+
+    // TODO: Amplia esta función para pintar la imagen ANTES de cada elemento LI
+
+    list.replaceChildren();
+
+    // Filters
+    let filtered = characters;
+    filtered = filtered.filter(i => i.image);
+
+    if (options.house) {
+        filtered = filtered.filter(i => i.house === options.house);
+    }
+
+    if (options.limit) {
+        filtered = filtered.slice(0, options.limit);
+    }
+
+    // End Filters
+
+    for (const character of filtered) {
+        const li = document.createElement('li');
+        li.textContent = `${character.name} - ${character.house || 'Sin Casa' }`;
+
+        // Foto:
+        const img = document.createElement('img');
+        img.src = character.image;
+        img.alt = character.name;
+        img.width = 50;
+        li.prepend(img);
+
+        list.appendChild(li);
+    }
+
+}
     // TODO: Crea una funcion que renderice los characters para poder eliminar el código de main.ts
 
     public async mount(): Promise<void> {
         document.title = this.title;
         console.log(`[Page] mounting home`);
-        await this.render();
+        this.state = { status: 'loading' };
+        this.render();
+        try {
+            const data = await getCharacters();
+            this.state = { status: 'success', data: data };
+        } catch (e: unknown) {
+            this.state = { status: 'error', message: e instanceof Error ? e.message : 'Unknown error' }
+        }
+        this.render();
     }
 
 }
