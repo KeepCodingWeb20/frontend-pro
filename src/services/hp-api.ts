5708e1580c7a072c3aada5fc4a06c3ccf49a8882
Nauel Gómez <ngomez@codiara.com>
2026-05-20 19:45:30 +0200

    Mapper implementado

--- src/services/hp-api.ts
+++ src/services/hp-api.ts
@@ -1,12 +1,19 @@
 import type { HPCharacter, HPSpell } from '../types/hp.types';
 import { apiGet } from '../lib/api';
+import type { HPCharacterDTO } from '../types/api.types';
+import { mapCharacter } from './mappers/character.mapper';
+import type { Character } from '../types/domain.types';
 
 const API_URL = 'https://hp-api.onrender.com/api';
 
 // apiGEt generico. -> cada funcion lo llame.
 export type CharacterFetcher = () => Promise<HPCharacter[]>;
 // TODO: consumir el mapper en getCharacters;
-export const getCharacters: CharacterFetcher = () => apiGet<HPCharacter[]>('/characters');
+//export const getCharacters: CharacterFetcher = () => apiGet<HPCharacter[]>('/characters');
+export async function getCharacters(): Promise<Character[]> {
+    const dtos = await apiGet<HPCharacterDTO[]>('/characters');
+    return dtos.map(mapCharacter);
+};
 
 export type SpellFetcher = () => Promise<HPSpell[]>;
 export const getSpells: SpellFetcher = () => apiGet<HPSpell[]>('/spells');
